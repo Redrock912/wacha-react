466814a11d4fd0b8a0304cd8f4907cc4e7e81e6f
Redrock912 <leset912@gmail.com>
2019-08-23 17:18:59 +0900

    playing with state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
+import { createSecureContext } from "tls";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // dynamic data -> state
+  state = {
+    count: 0
+  };
+
+  componentDidMount() {
+    console.log("render done");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  componentWillUnmount() {
+    console.log("I'm Free");
+  }
+
+  // React automatically returns render function
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>I'm a class {this.state.count}</h1>
+        <button onClick={this.addCount}>Add</button>
+        <button onClick={this.minusCount}>Minus</button>
+      </div>
+    );
+  }
+
+  addCount = () => {
+    console.log("add");
+    // setState tells react to call render() again with new state. if you don't use it render will not be called
+    this.setState(current => ({ count: current.count + 1 }));
+  };
+  minusCount = () => {
+    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
+  };
 }
 
 export default App;
